023391e921a50b4f823937e43453e4f7b5847765
Sergei Suslov <sergej.suslov@gmail.com>
2019-06-12 21:20:42 +0300

    Updates

---
+++ precache-manifest.74f84e4ad93d0c779972be04f18a8ed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6f47ba56f5185432c65e3e9f9776d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2800c048662d68648da9",
+    "url": "/static/css/main.0c768316.chunk.css"
+  },
+  {
+    "revision": "1f3dd14ace90bed7a052",
+    "url": "/static/js/2.16603809.chunk.js"
+  },
+  {
+    "revision": "2800c048662d68648da9",
+    "url": "/static/js/main.bca17f32.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
